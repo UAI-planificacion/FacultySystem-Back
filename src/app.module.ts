5ea5b59b3e5e08d670133faf12eea74f6261721d
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:50:18 -0400

    ConfigModule in App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { FacultiesModule } from './faculties/faculties.module';
-import { StaffModule } from './staff/staff.module';
-import { SubjectsModule } from './subjects/subjects.module';
+import { AppController }    from '@app/app.controller';
+import { FacultiesModule }  from '@faculties/faculties.module';
+import { StaffModule }      from '@staff/staff.module';
+import { SubjectsModule }   from '@subjects/subjects.module';
+import { ConfigModule }     from '@config/config.module';
+
 
 @Module({
-    imports     : [FacultiesModule, StaffModule, SubjectsModule],
+    imports     : [
+        FacultiesModule,
+        StaffModule,
+        SubjectsModule,
+        ConfigModule
+    ],
     controllers : [AppController],
     providers   : [],
 })
