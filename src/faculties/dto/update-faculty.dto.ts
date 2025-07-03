5ee4e537c48627edaf88232f590b3947b82a0d06
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:52:46 -0400

    Update Faculty DTO updated

--- src/faculties/dto/update-faculty.dto.ts
+++ src/faculties/dto/update-faculty.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType }  from '@nestjs/swagger';
+
 import { CreateFacultyDto } from './create-faculty.dto';
 
-export class UpdateFacultyDto extends PartialType(CreateFacultyDto) {}
+export class UpdateFacultyDto extends PartialType( CreateFacultyDto ) {}
