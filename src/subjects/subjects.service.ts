632fc8daf4b6a34fe8d8fc2ed9888ea4f8b8f7f2
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:54:07 -0400

    Subject services updated

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSubjectDto } from './dto/create-subject.dto';
-import { UpdateSubjectDto } from './dto/update-subject.dto';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+
+import { PrismaException }  from '@app/config/prisma-catch';
+import { CreateSubjectDto } from '@subjects/dto/create-subject.dto';
+import { UpdateSubjectDto } from '@subjects/dto/update-subject.dto';
+
 
 @Injectable()
-export class SubjectsService {
-  create(createSubjectDto: CreateSubjectDto) {
-    return 'This action adds a new subject';
-  }
+export class SubjectsService extends PrismaClient implements OnModuleInit {
+    onModuleInit() {
+        this.$connect();
+    }
 
-  findAll() {
-    return `This action returns all subjects`;
-  }
+    async create( createSubjectDto: CreateSubjectDto ) {
+        try {
+            return await this.subject.create({
+                data: createSubjectDto,
+            });
+        } catch (error) {
+            throw PrismaException.catch(error, 'Failed to create subject');
+        }
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subject`;
-  }
+    async findAll(facultyId: string) {
+        return await this.subject.findMany({
+            where: { facultyId }
+        });
+    }
 
-  update(id: number, updateSubjectDto: UpdateSubjectDto) {
-    return `This action updates a #${id} subject`;
-  }
+    async findOne(id: string) {
+        const subject = await this.subject.findUnique({ 
+            where: { id },
+        });
 
-  remove(id: number) {
-    return `This action removes a #${id} subject`;
-  }
+        if (!subject) {
+            throw new NotFoundException('Subject not found');
+        }
+
+        return subject;
+    }
+
+    async update(id: string, updateSubjectDto: UpdateSubjectDto) {
+        try {
+            return await this.subject.update({
+                where: { id },
+                data: {
+                    name            : updateSubjectDto.name,
+                    startDate       : updateSubjectDto.startDate,
+                    endDate         : updateSubjectDto.endDate,
+                    students        : updateSubjectDto.students,
+                    costCenterId    : updateSubjectDto.costCenterId
+                }
+            });
+        } catch (error) {
+            throw PrismaException.catch(error, 'Failed to update subject');
+        }
+    }
+
+    async remove(id: string) {
+        try {
+            return await this.subject.delete({ where: { id } });
+        } catch (error) {
+            throw PrismaException.catch(error, 'Failed to delete subject');
+        }
+    }
 }
