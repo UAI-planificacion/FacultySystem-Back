9e8b26fdac3a585e66de47e4f8f8689d5589f17c
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:53:56 -0400

    Subjet controller updated

--- src/subjects/subjects.controller.ts
+++ src/subjects/subjects.controller.ts
@@ -1,34 +1,48 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
 import { SubjectsService } from './subjects.service';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 
 @Controller('subjects')
 export class SubjectsController {
-  constructor(private readonly subjectsService: SubjectsService) {}
+    constructor(
+        private readonly subjectsService: SubjectsService
+    ) {}
 
-  @Post()
-  create(@Body() createSubjectDto: CreateSubjectDto) {
-    return this.subjectsService.create(createSubjectDto);
-  }
+    @Post()
+    create(
+        @Body() createSubjectDto: CreateSubjectDto
+    ) {
+        return this.subjectsService.create(createSubjectDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.subjectsService.findAll();
-  }
+    @Get('all/:facultyId')
+    findAll(
+        @Param('facultyId') facultyId: string
+    ) {
+        return this.subjectsService.findAll(facultyId);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.subjectsService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(
+        @Param('id') id: string
+    ) {
+        return this.subjectsService.findOne(id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSubjectDto: UpdateSubjectDto) {
-    return this.subjectsService.update(+id, updateSubjectDto);
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updateSubjectDto: UpdateSubjectDto
+    ) {
+        return this.subjectsService.update(id, updateSubjectDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.subjectsService.remove(+id);
-  }
+    @Delete(':id')
+    remove(
+        @Param('id') id: string
+    ) {
+        return this.subjectsService.remove(id);
+    }
 }
