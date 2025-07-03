a9acf573064bd93b516df7be7bd91cf1c8f8766f
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:51:50 -0400

    Faculties controller updated

--- src/faculties/faculties.controller.ts
+++ src/faculties/faculties.controller.ts
@@ -1,34 +1,52 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
 import { FacultiesService } from './faculties.service';
 import { CreateFacultyDto } from './dto/create-faculty.dto';
 import { UpdateFacultyDto } from './dto/update-faculty.dto';
 
-@Controller('faculties')
+@Controller( 'faculties' )
 export class FacultiesController {
-  constructor(private readonly facultiesService: FacultiesService) {}
+    constructor(
+        private readonly facultiesService: FacultiesService
+    ) {}
 
-  @Post()
-  create(@Body() createFacultyDto: CreateFacultyDto) {
-    return this.facultiesService.create(createFacultyDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.facultiesService.findAll();
-  }
+    @Post()
+    create(
+        @Body() createFacultyDto: CreateFacultyDto
+    ) {
+        return this.facultiesService.create( createFacultyDto );
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.facultiesService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFacultyDto: UpdateFacultyDto) {
-    return this.facultiesService.update(+id, updateFacultyDto);
-  }
+    @Get()
+    findAll() {
+        return this.facultiesService.findAll();
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.facultiesService.remove(+id);
-  }
+
+    @Get( ':id' )
+    findOne(
+        @Param( 'id' ) id: string
+    ) {
+        return this.facultiesService.findOne( id );
+    }
+
+
+    @Patch( ':id' )
+    update(
+        @Param( 'id' ) id: string,
+        @Body() updateFacultyDto: UpdateFacultyDto
+    ) {
+        return this.facultiesService.update( id, updateFacultyDto );
+    }
+
+
+    @Delete( ':id' )
+    remove(
+        @Param( 'id' ) id: string
+    ) {
+        return this.facultiesService.remove( id );
+    }
+
 }
