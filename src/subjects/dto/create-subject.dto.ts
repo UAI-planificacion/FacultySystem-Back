06c51062f021abfc39243861f4b8081afefb3dd4
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:54:30 -0400

    Create Subject DTO updated

--- src/subjects/dto/create-subject.dto.ts
+++ src/subjects/dto/create-subject.dto.ts
@@ -1,1 +1,9 @@
-export class CreateSubjectDto {}
+import { IntersectionType } from '@nestjs/swagger';
+
+import { FacultyIdDto } from '@app/faculties/dto/faculty-id.dto';
+import { BasicSubjectDto } from './basic-subject.dto';
+
+export class CreateSubjectDto extends IntersectionType(
+    BasicSubjectDto,
+    FacultyIdDto,
+) {}
