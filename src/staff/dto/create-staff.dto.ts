c6d6d1a2260a45482ba42c3e6143f09a751b1533
kevinkeyssx <keyss1700@gmail.com>
2025-07-03 10:53:34 -0400

    Create Staff DTO updated

--- src/staff/dto/create-staff.dto.ts
+++ src/staff/dto/create-staff.dto.ts
@@ -1,1 +1,7 @@
-export class CreateStaffDto {}
+import { IntersectionType } from '@nestjs/swagger';
+
+import { FacultyIdDto }     from '@app/faculties/dto/faculty-id.dto';
+import { BasicStaffDto }    from '@staff/dto/basic-staff.dto';
+
+
+export class CreateStaffDto extends IntersectionType( BasicStaffDto, FacultyIdDto ) {}
