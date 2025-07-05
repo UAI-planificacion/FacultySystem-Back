a667daecaf463ab4126a157a1d1595c302a17751
kevinkeyssx <keyss1700@gmail.com>
2025-07-04 22:17:38 -0400

    Basic Request DTO added

---
+++ src/requests/dto/basic-request.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+import {
+    IsBoolean,
+    IsEnum,
+    IsOptional,
+    IsString,
+    Length,
+}                   from 'class-validator';
+import { Status }   from 'generated/prisma';
+
+
+export class BasicRequestDto {
+
+    @ApiPropertyOptional({ 
+        description : 'Request status',
+        enum        : Object.values( Status ),
+        default     : Status.PENDING
+    })
+    @IsString()
+    @IsOptional()
+    @IsEnum( Status )
+    status: Status = Status.PENDING;
+
+    @ApiPropertyOptional({ 
+        description : 'Whether the request is for consecutive days',
+        default     : false 
+    })
+    @IsOptional()
+    @IsBoolean()
+    isConsecutive: boolean = false;
+
+    @ApiPropertyOptional({ 
+        description : 'Description of the request',
+        example     : 'Request for classroom with projector'
+    })
+    @IsOptional()
+    @IsString()
+    @Length( 0, 255 )
+    description?: string;
+
+    @ApiProperty({ 
+        description : 'ID of the subject this request is for',
+        example     : '01H9XKJ8WXKJ8WXKJ8WXKJ8WX'
+    })
+    @IsString()
+    @Length( 26, 26 )
+    subjectId: string;
+
+}
