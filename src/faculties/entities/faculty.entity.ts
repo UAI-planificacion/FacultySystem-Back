aebe8cb85c8265438f88c15e4a589fd07f02dab5
kevinkeyssx <keyss1700@gmail.com>
2025-07-07 21:52:50 -0400

    Faculty Responde added

--- src/faculties/entities/faculty.entity.ts
+++ src/faculties/entities/faculty.entity.ts
@@ -1,1 +1,19 @@
-export class Faculty {}
+export class FacultyResponse {
+    totalSubjects   : number;
+    totalPersonnel  : number;
+    totalRequests   : number;
+    faculties       : Faculty[];
+}
+
+
+export class Faculty {
+    id              : string;
+    name            : string;
+    description?    : string | null;
+    isActive        : boolean;
+    totalSubjects   : number;
+    totalPersonnel  : number;
+    totalRequests   : number;
+    createdAt       : Date;
+    updatedAt       : Date;
+}
