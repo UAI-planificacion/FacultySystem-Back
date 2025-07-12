9acb9b0aaa62a23f6f3cac0a0c146844998f5351
kevinkeyssx <keyss1700@gmail.com>
2025-07-11 20:18:27 -0400

    BasicRequestDetailDto implemented

--- src/request-details/dto/create-request-detail.dto.ts
+++ src/request-details/dto/create-request-detail.dto.ts
@@ -1,136 +1,14 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
 import {
-    IsArray,
-    IsBoolean,
-    IsEnum,
-    IsInt,
-    IsOptional,
     IsString,
-    IsUUID,
     Length,
-    Max,
-    Min
-}               from 'class-validator';
-import { Type } from 'class-transformer';
+}from 'class-validator';
 
-import { Level, Size, SpaceType, Building } from 'generated/prisma';
+import { BasicRequestDetailDto } from './basic-request-detail.dto';
 
 
-export class CreateRequestDetailDto {
-
-    @ApiPropertyOptional({
-        description : 'Minimum number of students',
-        example     : 20
-    })
-    @IsOptional()
-    @IsInt()
-    @Min( 0 )
-    @Max( 1000 )
-    @Type( () => Number )
-    minimum?: number;
-
-    @ApiPropertyOptional({
-        description : 'Maximum number of students',
-        example     : 30
-    })
-    @IsOptional()
-    @IsInt()
-    @Min( 0 )
-    @Max( 1000 )
-    @Type( () => Number )
-    maximum?: number;
-
-    @ApiPropertyOptional({
-        enum        : Object.values( SpaceType ),
-        description : 'Type of space required'
-    })
-    @IsOptional()
-    @IsString()
-    @IsEnum( SpaceType )
-    spaceType?: SpaceType;
-
-    @ApiPropertyOptional({
-        enum        : Object.values( Size ),
-        description : 'Size of the space'
-    })
-    @IsOptional()
-    @IsString()
-    @IsEnum( Size )
-    spaceSize?: Size;
-
-    @ApiPropertyOptional({ description: 'Cost center ID' })
-    @IsOptional()
-    @IsString()
-    @Length( 0, 255 )
-    costCenterId?: string;
-
-    @ApiPropertyOptional({
-        description : 'If the request is for the afternoon',
-        default     : false
-    })
-    @IsOptional()
-    @IsBoolean()
-    inAfternoon: boolean = false;
-
-    @ApiPropertyOptional({
-        enum        : Object.values( Building ),
-        description : 'Building name or identifier'
-    })
-    @IsOptional()
-    @IsString()
-    @IsEnum( Building )
-    building?: Building;
-
-    @ApiPropertyOptional({ description: 'Additional description' })
-    @IsOptional()
-    @IsString()
-    @Length( 0, 255 )
-    description?: string;
-
-    @ApiPropertyOptional({ description: 'Module ID' })
-    @IsOptional()
-    @IsUUID()
-    moduleId?: string;
-
-    @ApiPropertyOptional({
-        type        : [String],
-        description : 'Days of the week'
-    })
-    @IsOptional()
-    @IsArray()
-    @IsString({ each: true })
-    days: string[] = [];
-
-    @ApiPropertyOptional({ description: 'Space ID' })
-    @IsOptional()
-    @IsString()
-    @Length( 0, 255 )
-    spaceId?: string;
-
-    @ApiPropertyOptional({
-        description : 'If this is a priority request',
-        default     : false
-    })
-    @IsOptional()
-    @IsBoolean()
-    isPriority: boolean = false;
-
-    @ApiPropertyOptional({ 
-        enum        : Object.values( Level ),
-        default     : Level.PREGRADO,
-        description : 'Education level'
-    })
-    @IsOptional()
-    @IsString()
-    @IsEnum( Level )
-    nivel: Level = Level.PREGRADO;
-
-    @ApiPropertyOptional({ description: 'Professor ID' })
-    @IsOptional()
-    @IsString()
-    @Length( 0, 26 )
-    professorId?: string;
+export class CreateRequestDetailDto extends BasicRequestDetailDto {
 
     @ApiProperty({ description: 'Request ID' })
     @IsString()
