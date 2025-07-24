c84d134470fcf86502a4d0a5c1053203fec80bd0
kevinkeyssx <keyss1700@gmail.com>
2025-07-23 22:25:31 -0400

    StaffUpdateIdDTO implemented

--- src/requests/dto/update-request.dto.ts
+++ src/requests/dto/update-request.dto.ts
@@ -1,20 +1,13 @@
-import { ApiPropertyOptional, IntersectionType } from '@nestjs/swagger';
+import { IntersectionType } from '@nestjs/swagger';
 
-import { IsOptional, IsString, Length } from 'class-validator';
 
 import { BasicRequestDto }  from '@requests/dto/basic-request.dto';
 import { CommentDto }       from '@requests/dto/comment.dto';
+import { StaffUpdateIdDTO } from '@requests/dto/staff-update-id.dto';
 
 
-export class UpdateRequestDto extends IntersectionType( BasicRequestDto, CommentDto ) {
-
-    @ApiPropertyOptional({
-        description : 'ID of the staff member creating the request',
-        example     : '01H9XKJ8WXKJ8WXKJ8WXKJ8WX'
-    })
-    @IsString()
-    @Length( 26, 26 )
-    @IsOptional()
-    staffUpdatedId?: string;
-
-}
+export class UpdateRequestDto extends IntersectionType(
+    BasicRequestDto,
+    CommentDto,
+    StaffUpdateIdDTO
+) {}
