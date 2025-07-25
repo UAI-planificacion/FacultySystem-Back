5058626980712105e8f5942020d1fd40ffd7409d
kevinkeyssx <keyss1700@gmail.com>
2025-07-24 22:26:49 -0400

    BasicSubjectDto updated

--- src/subjects/dto/basic-subject.dto.ts
+++ src/subjects/dto/basic-subject.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 import { IsDateString, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class BasicSubjectDto {
