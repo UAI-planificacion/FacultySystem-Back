c2ddda2135b180174adb95c63192a67c1b88e909
kevinkeyssx <keyss1700@gmail.com>
2025-07-24 22:25:42 -0400

    Comment controller added

---
+++ src/comments/comments.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+import { CommentsService }  from '@comments/comments.service';
+import { CreateCommentDto } from '@comments/dto/create-comment.dto';
+import { UpdateCommentDto } from '@comments/dto/update-comment.dto';
+
+
+@Controller( 'comments' )
+export class CommentsController {
+
+    constructor(
+        private readonly commentService: CommentsService
+    ) {}
+
+    @Post()
+    create(
+        @Body() createCommentDto: CreateCommentDto
+    ) {
+        return this.commentService.create( createCommentDto );
+    }
+
+    @Get( 'request/:id' )
+    findAllByRequestId(
+        @Param('id') id: string,
+    ) {
+        return this.commentService.findAllByRequestId( id );
+    }
+
+
+    @Get( 'request-detail/:id' )
+    findAllByRequestDetailId(
+        @Param( 'id' ) id: string
+    ) {
+        return this.commentService.findAllByRequestDetailId( id );
+    }
+
+    @Get( ':id' )
+    findOne(
+        @Param( 'id' ) id: string
+    ) {
+        return this.commentService.findOne( id );
+    }
+
+    @Patch( ':id' )
+    update(
+        @Param( 'id' ) id: string,
+        @Body() updateCommentDto: UpdateCommentDto
+    ) {
+        return this.commentService.update( id, updateCommentDto );
+    }
+
+    @Delete( ':id' )
+    remove(
+        @Param( 'id' ) id: string
+    ) {
+        return this.commentService.remove( id );
+    }
+
+}
