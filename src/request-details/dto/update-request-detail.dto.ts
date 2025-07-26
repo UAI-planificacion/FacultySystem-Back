63de0073ea88152ed1cc21f9e24b55d2cfd657cc
kevinkeyssx <keyss1700@gmail.com>
2025-07-25 21:51:11 -0400

    CommentDto removed

--- src/request-details/dto/update-request-detail.dto.ts
+++ src/request-details/dto/update-request-detail.dto.ts
@@ -1,12 +1,10 @@
 import { IntersectionType } from '@nestjs/swagger';
 
-import { CommentDto }               from '@requests/dto/comment.dto';
 import { BasicRequestDetailDto }    from '@request-details/dto/basic-request-detail.dto';
-import { StaffUpdateIdDTO }           from '@requests/dto/staff-update-id.dto';
+import { StaffUpdateIdDTO }         from '@requests/dto/staff-update-id.dto';
 
 
 export class UpdateRequestDetailDto extends IntersectionType(
     BasicRequestDetailDto,
-    CommentDto,
     StaffUpdateIdDTO
 ) {}
