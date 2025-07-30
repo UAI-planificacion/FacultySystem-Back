425e8dec70098d3d8c96ef7cba42def868bcd60c
kevinkeyssx <keyss1700@gmail.com>
2025-07-29 22:39:23 -0400

    SseModule imported

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { RequestsService }    from '@requests/requests.service';
-import { RequestsController } from '@requests/requests.controller';
+import { RequestsService }      from '@requests/requests.service';
+import { RequestsController }   from '@requests/requests.controller';
+import { SseModule }            from '@sse/sse.module';
 
 @Module({
     controllers : [RequestsController],
     providers   : [RequestsService],
+    imports     : [SseModule],
 })
 export class RequestsModule {}
