b34b216af99684f0d70c727041d1b6a306100bfc
kevinkeyssx <keyss1700@gmail.com>
2025-08-12 20:50:15 -0400

    CreateGradeDto added

--- src/grades/dto/create-grade.dto.ts
+++ src/grades/dto/create-grade.dto.ts
@@ -1,1 +1,24 @@
-export class CreateGradeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+import { IsString, Length } from 'class-validator';
+
+
+export class CreateGradeDto {
+
+    @ApiProperty({ 
+        description : 'Name of the grade',
+        example     : 'Grade 1'
+    })
+    @IsString()
+    @Length( 3, 50 )
+    name: string;
+
+    @ApiProperty({ 
+        description : 'Id of the headquarter',
+        example     : '1'
+    })
+    @IsString()
+    @Length( 1, 50 )
+    headquartersId : string;
+
+}
