3425661a8f3ca981e6099be5500817755a0d83c3
kevinkeyssx <keyss1700@gmail.com>
2025-08-15 22:01:13 -0400

    staffUpdateId is required

--- src/requests/dto/staff-update-id.dto.ts
+++ src/requests/dto/staff-update-id.dto.ts
@@ -1,17 +1,16 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
-import { IsOptional, IsString, Length } from 'class-validator';
+import { IsString, Length } from 'class-validator';
 
 
 export class StaffUpdateIdDTO {
 
-    @ApiPropertyOptional({
-        description : 'ID of the staff member creating the request',
+    @ApiProperty({
+        description : 'ID of the staff member updating the request',
         example     : '01H9XKJ8WXKJ8WXKJ8WXKJ8WX'
     })
     @IsString()
-    @IsOptional()
     @Length( 23, 28 )
-    staffUpdateId?: string;
+    staffUpdateId: string;
 
 }
