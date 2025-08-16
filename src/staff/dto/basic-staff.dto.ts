5a7b1f781b46ce99f5e4e79dbb09b3ad7d95a1be
kevinkeyssx <keyss1700@gmail.com>
2025-08-15 22:01:47 -0400

    Tabs & spaces added

--- src/staff/dto/basic-staff.dto.ts
+++ src/staff/dto/basic-staff.dto.ts
@@ -1,34 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsEmail, IsNotEmpty, IsEnum, Length } from 'class-validator';
+
 import { Role } from 'generated/prisma';
+
+import { IsString, IsEmail, IsNotEmpty, IsEnum, Length } from 'class-validator';
 
 
 export class BasicStaffDto {
 
     @ApiProperty({
-        description: 'The name of the staff member',
-        example: 'John Doe',
+        description : 'The name of the staff member',
+        example     : 'John Doe',
     })
     @IsString()
     @IsNotEmpty()
-    @Length(3, 100)
+    @Length( 3, 100 )
     name: string;
 
     @ApiProperty({
-        description: 'The email of the staff member',
-        example: 'john.doe@example.com',
+        description : 'The email of the staff member',
+        example     : 'john.doe@example.com',
     })
     @IsEmail()
     @IsNotEmpty()
-    @Length(3, 100)
+    @Length( 3, 100 )
     email: string;
 
     @ApiProperty({
-        description: 'The role of the staff member',
-        enum: Role,
-        example: Role.VIEWER,
+        description : 'The role of the staff member',
+        enum        : Role,
+        example     : Role.VIEWER,
     })
-    @IsEnum(Role)
+    @IsEnum( Role )
     @IsNotEmpty()
     role: Role;
 
