ea7e940c564b5ca0b059665706467e584c55a5cc
kevinkeyssx <keyss1700@gmail.com>
2025-08-15 22:01:01 -0400

    ContentCommentDTO implemented

--- src/comments/dto/update-comment.dto.ts
+++ src/comments/dto/update-comment.dto.ts
@@ -1,16 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ContentCommentDTO } from '@comments/dto/content.dto';
 
 
-export class UpdateCommentDto {
-
-    @ApiProperty({
-		description	: 'Content of the comment',
-		example		: 'This is a comment about the request'
-	})
-	@IsString()
-	@IsNotEmpty()
-	content: string;
-
-}
+export class UpdateCommentDto  extends ContentCommentDTO {}
