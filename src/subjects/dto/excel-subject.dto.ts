22647692006f81ad61f0430c24861f6724ab1ace
kevinkeyssx <keyss1700@gmail.com>
2025-08-25 22:50:49 -0400

    BasicSubjectDto implemented

--- src/subjects/dto/excel-subject.dto.ts
+++ src/subjects/dto/excel-subject.dto.ts
@@ -2,15 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import {
 	IsArray,
-	IsBoolean,
-	IsDate,
-	IsInt,
-	IsNotEmpty,
-	IsOptional,
-	IsString,
 	ValidateNested
-}				from 'class-validator';
-import { Type }		from 'class-transformer';
+}                                       from 'class-validator';
+import { Type }                         from 'class-transformer';
+import { Building, Size, SpaceType }    from 'generated/prisma';
+
+import { BasicSubjectDto } from '@subjects/dto/basic-subject.dto';
 
 
 /**
@@ -19,12 +16,15 @@ import { Type }		from 'class-transformer';
 export interface IExcelSubject {
 	id				: string;
 	name			: string;
-	startDate		: string[];
-	endDate			: string[];
+	startDate		: string[] | Date[];
+	endDate			: string[] | Date[];
 	students		: number;
 	costCenterId	: string;
 	isEnglish		: boolean;
 	facultyId		: string;
+    building?       : Building | undefined;
+    spaceType?      : SpaceType | undefined;
+    spaceSize?      : Size |undefined;
 }
 
 
@@ -39,81 +39,7 @@ export class BulkCreateSubjectDto {
 	})
 	@IsArray()
 	@ValidateNested({ each: true })
-	@Type( () => ExcelSubjectDto )
-	subjects: ExcelSubjectDto[];
-
-}
-
-
-/**
- * DTO for individual subject from Excel data
- */
-export class ExcelSubjectDto {
-
-	@ApiProperty({
-		description	: 'The id of the subject',
-		example		: 'sub-1234567890',
-	})
-	@IsString()
-	@IsNotEmpty()
-	id: string;
-
-	@ApiProperty({
-		description	: 'The name of the subject',
-		example		: 'Mathematics 101',
-	})
-	@IsString()
-	@IsNotEmpty()
-	name: string;
-
-	@ApiProperty({
-		description	: 'The start date of the subject as string array',
-		example		: ['2025-08-01', '2025-09-01'],
-	})
-	@IsOptional()
-	@IsArray()
-	@IsString({ each: true })
-	startDate: string[] = [];
-
-	@ApiProperty({
-		description	: 'The end date of the subject as string array',
-		example		: ['2025-12-15', '2025-12-20'],
-	})
-	@IsOptional()
-	@IsArray()
-	@IsString({ each: true })
-	endDate: string[] = [];
-
-	@ApiProperty({
-		description	: 'Number of students in the subject',
-		example		: 30,
-	})
-	@IsInt()
-	@IsNotEmpty()
-	students: number;
-
-	@ApiProperty({
-		description	: 'The cost center ID for the subject',
-		example		: 'CC-2025-MATH-101',
-	})
-	@IsString()
-	@IsNotEmpty()
-	costCenterId: string;
-
-	@ApiProperty({
-		description	: 'Is English subject',
-		example		: true
-	})
-	@IsOptional()
-	@IsBoolean()
-	isEnglish?: boolean;
-
-	@ApiProperty({
-		description	: 'Faculty ID',
-		example		: '01H9XKJ8WXKJ8WXKJ8WXKJ8WX'
-	})
-	@IsString()
-	@IsNotEmpty()
-	facultyId: string;
+	@Type( () => BasicSubjectDto )
+	subjects: BasicSubjectDto[];
 
 }
