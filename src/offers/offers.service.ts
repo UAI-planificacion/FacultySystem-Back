e2b1aa2145863329bca9f4fa0e7f5336014a532a
kevinkeyssx <keyss1700@gmail.com>
2025-09-12 21:52:37 -0300

    OffersService added

---
+++ src/offers/offers.service.ts
@@ -0,0 +1,89 @@
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+
+import { PrismaClient } from 'generated/prisma';
+
+import { PrismaException }  from '@app/config/prisma-catch';
+import { CreateOfferDto }   from './dto/create-offer.dto';
+import { UpdateOfferDto }   from './dto/update-offer.dto';
+
+
+@Injectable()
+export class OffersService extends PrismaClient implements OnModuleInit {
+
+    onModuleInit() {
+        this.$connect();
+    }
+
+
+    #validDates( startDate: Date[] | undefined, endDate: Date[] | undefined ): void {
+        if ( startDate?.length !== endDate?.length ) {
+            throw new BadRequestException({
+                code    : 'OFFER-002',
+                message : 'Start date and end date must have the same length',
+            });
+        }
+    }
+
+
+    async create( createOfferDto: CreateOfferDto ) {
+        if ( !createOfferDto.workshop
+            && !createOfferDto.laboratory
+            && !createOfferDto.tutoringSession
+            && !createOfferDto.lecture
+        ) {
+            // 'At least one of the following must be filled: workshop, laboratory, tutoringSession'
+            throw new BadRequestException({
+                code    : 'OFFER-001',
+                message : 'At least one of the following must be filled: workshop, laboratory, tutoringSession',
+            });
+        }
+
+        this.#validDates( createOfferDto.startDate, createOfferDto.endDate );
+
+        try {
+            return await this.offer.create({
+                data: createOfferDto,
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to create subject' );
+        }
+    }
+
+
+    findAll() {
+        return this.offer.findMany();
+    }
+
+
+    findOne( id: string ) {
+        return this.offer.findUnique({
+            where: { id }
+        });
+    }
+
+
+    update( id: string, updateOfferDto: UpdateOfferDto ) {
+        this.#validDates( updateOfferDto.startDate, updateOfferDto.endDate );
+
+        try {
+            return this.offer.update({
+                where   : { id },
+                data    : updateOfferDto,
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to update offer' );
+        }
+    }
+
+
+    async remove( id: string ) {
+        try {
+            return await this.offer.delete({
+                where: { id }
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to delete offer' );
+        }
+    }
+
+}
