30bd2a1413dd86dfca6dafd3427a8de01526c568
kevinkeyssx <keyss1700@gmail.com>
2025-09-29 21:37:31 -0300

    CommunFilefsSectionDto not  working

--- src/sections/dto/commun-fields.dto.ts
+++ src/sections/dto/commun-fields.dto.ts
@@ -8,16 +8,16 @@ import { CodeDto } from '@sections/dto/fields/code.dto';
 
 export class CommunFilefsSectionDto extends CodeDto {
 
-    @ApiProperty({
-        description : 'Session identifier for the section (e.g., "A", "B", "Virtual").',
-        example     : $Enums.Session.C,
-        maxLength   : 1,
-    })
-    @IsEnum($Enums.Session, {
-        message: `Session must be one of the following values: ${Object.values(
-            $Enums.Session,
-        ).join( ', ' )}`,
-    })
-    session: $Enums.Session;
+    // @ApiProperty({
+    //     description : 'Session identifier for the section (e.g., "A", "B", "Virtual").',
+    //     example     : $Enums.Session.C,
+    //     maxLength   : 1,
+    // })
+    // @IsEnum($Enums.Session, {
+    //     message: `Session must be one of the following values: ${Object.values(
+    //         $Enums.Session,
+    //     ).join( ', ' )}`,
+    // })
+    // session: $Enums.Session;
 
 }
