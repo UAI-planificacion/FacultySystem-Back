7d3b4fe24c021c68f7d76424ca990914111376f9
kevinkeyssx <keyss1700@gmail.com>
2025-10-01 21:57:02 -0300

    BasicSectionDto updated

--- src/sections/dto/basic-section.dto.ts
+++ src/sections/dto/basic-section.dto.ts
@@ -1,9 +1,7 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger';
 import {
-    IsInt,
-    Min,
-    Max,
     IsNotEmpty,
+    IsDate,
 }               from 'class-validator';
 import { Type } from 'class-transformer';
 
@@ -18,15 +16,22 @@ export class BasicSectionDto extends IntersectionType(
     SpaceTypeDto
 ) {
 
-    // @ApiProperty({
-    //     description: 'The code of the section.',
-    //     example: 101,
-    // })
-    // @Type(() => Number)
-    // @IsInt({ message: 'Section code must be an integer.' })
-    // @Min(1, { message: 'Section code must be a positive integer.' })
-    // @Max( 1000, { message: 'Section code cannot exceed 1000.' } )
-    // @IsNotEmpty({ message: 'Section code is required.' })
-    // code: number;
+    @ApiProperty({
+        description: 'The start date of the section.',
+        example: '2025-01-01',
+    })
+    @Type(() => Date)
+    @IsDate({ message: 'Section start date must be a valid date.' })
+    @IsNotEmpty({ message: 'Section start date is required.' })
+    startDate: Date;
+
+    @ApiProperty({
+        description: 'The end date of the section.',
+        example: '2025-12-31',
+    })
+    @Type(() => Date)
+    @IsDate({ message: 'Section end date must be a valid date.' })
+    @IsNotEmpty({ message: 'Section end date is required.' })
+    endDate: Date;
 
 }
