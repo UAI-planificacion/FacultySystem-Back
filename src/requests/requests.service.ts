18993757c8596693de8b55a33c3b20c3e5c3dcf8
kevinkeyssx <keyss1700@gmail.com>
2025-10-08 22:29:01 -0300

    All services uncommented

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, NotFoundException } from '@nestjs/common';
 
-import { Prisma, PrismaClient } from 'generated/prisma';
+import { PrismaClient } from 'generated/prisma';
 
 import { PrismaException }  from '@config/prisma-catch';
 import { CreateRequestDto } from '@requests/dto/create-request.dto';
@@ -24,201 +24,311 @@ export class RequestsService extends PrismaClient implements OnModuleInit {
     }
 
 
-    // #requestMap = ( request ) => ({
-    //     id              : request.id,
-    //     title           : request.title,
-    //     status          : request.status,
-    //     isConsecutive   : request.isConsecutive,
-    //     description     : request.description,
-    //     createdAt       : request.createdAt,
-    //     updatedAt       : request.updatedAt,
-    //     staffCreate     : request.staffCreate,
-    //     staffUpdate     : request.staffUpdate,
-    //     offer           : request.offer,
-    //     totalDetails    : request._count.details,
-    //     facultyId       : request.staffCreate.facultyId
-    // });
+    #requestMap = ( request ) => ({
+        id              : request.id,
+        title           : request.title,
+        status          : request.status,
+        createdAt       : request.createdAt,
+        updatedAt       : request.updatedAt,
+        staffCreate     : request.staffCreate,
+        staffUpdate     : request.staffUpdate,
+        totalDetails    : request._count.requesSessions,
+        facultyId       : request.staffCreate.facultyId,
+        section         : {
+            id          : request.section.id,
+            code        : request.section.code,
+            startDate   : request.section.startDate,
+            endDate     : request.section.endDate,
+            isClosed    : request.section.isClosed,
+            laboratory  : request.section.laboratory,
+            workshop    : request.section.workshop,
+            lecture     : request.section.lecture,
+            professor   : {
+                id      : request.section.professor.id,
+                name    : request.section.professor.name
+            },
+            period      : {
+                id      : request.section.period.id,
+                name    : request.section.period.name
+            },
+            subject     : {
+                id      : request.section.subject.id,
+                name    : request.section.subject.name
+            },
+            countSessions: {
+                sessions: request.section._count.sessions
+            }
+        },
+    });
 
 
-    // #selectRequest = {
-    //     id              : true,
-    //     title           : true,
-    //     status          : true,
-    //     isConsecutive   : true,
-    //     description     : true,
-    //     createdAt       : true,
-    //     updatedAt       : true,
-    //     staffCreate     : {
-    //         select: {
-    //             id          : true,
-    //             name        : true,
-    //             email       : true,
-    //             role        : true,
-    //             facultyId   : true
-    //         }
-    //     },
-    //     staffUpdate     : {
-    //         select: {
-    //             id      : true,
-    //             name    : true,
-    //             email   : true,
-    //             role    : true
-    //         }
-    //     },
-    //     offer : {
-    //         select : {
-    //             id              : true,
-    //             workshop        : true,
-    //             lecture         : true,
-    //             tutoringSession : true,
-    //             laboratory      : true,
-    //             spaceType       : true,
-    //             costCenterId    : true,
-    //             isEnglish       : true,
-    //             building        : true,
-    //             startDate       : true,
-    //             endDate         : true,
-    //             spaceSize : {
-    //                 select : {
-    //                     id      : true,
-    //                     detail  : true
-    //                 }
-    //             },
-    //             subject         : {
-    //                 select : {
-    //                     id      : true,
-    //                     name    : true,
-    //                 }
-    //             },
-    //             period : {
-    //                 select : {
-    //                     id      : true,
-    //                     name    : true
-    //                 }
-    //             }
-    //         }
-    //     },
-    //     _count: {
-    //         select: {
-    //             details: true
-    //         }
-    //     }
-    // }
+    #requestSectionMap = ( request  ) => ({
+        ...request,
+        requestSessions : request.requestSessions.map(( requestSession ) => ({
+            ...requestSession,
+            sessionDayModules   : requestSession.sessionDayModules.map(( sessionDayModule ) => ({
+                id          : sessionDayModule.id,
+                dayId       : sessionDayModule.dayModule.dayId,
+                module    : sessionDayModule.dayModule.module,
+            }))
+        }))
+    });
 
 
-    // async create(
-    //     createRequestDto    : CreateRequestDto,
-    //     origin              : string | undefined
-    // ) {
-    //     try {
-    //         const request       = await this.request.create({ data: createRequestDto });
-    //         const requestData   = await this.findOne( request.id );
-
-    //         this.sseService.emitEvent({
-    //             message : requestData,
-    //             action  : EnumAction.CREATE,
-    //             type    : Type.REQUEST,
-    //             origin
-    //         });
-
-    //         return requestData;
-    //     } catch ( error ) {
-    //         throw PrismaException.catch( error, 'Failed to create request' );
-    //     }
-    // }
-
-
-    // async findAll( facultyId: string ) {
-    //     const requests = await this.request.findMany({
-    //         select  : this.#selectRequest,
-    //         where   : {
-    //             offer : {
-    //                 subject : {
-    //                     facultyId
-    //                 }
-    //             }
-    //         }
-    //     });
-
-    //     return requests.map(( request ) => this.#requestMap( request ));
-    // }
-
-
-    // async findOne( id: string ) {
-    //     const request = await this.request.findUnique({
-    //         select  : this.#selectRequest,
-    //         where   : { id },
-    //     });
-
-    //     if ( !request ) {
-    //         throw new NotFoundException( 'Request not found' );
-    //     }
-
-    //     return this.#requestMap( request );
-    // }
-
-
-    // async update(
-    //     id                  : string,
-    //     updateRequestDto    : UpdateRequestDto,
-    //     origin              : string | undefined
-    // ) {
-    //     const data: Prisma.RequestUpdateInput = { ...updateRequestDto };
-
-    //     if ( updateRequestDto.offerId !== undefined ) {
-    //         delete ( data as any ).offerId; 
-
-    //         data.offer = {
-    //             connect: { id: updateRequestDto.offerId }
-    //         };
-    //     }
-
-    //     if ( updateRequestDto.staffUpdateId ) {
-    //         delete ( data as any ).staffUpdateId;
-
-    //         data.staffUpdate = {
-    //             connect: { id: updateRequestDto.staffUpdateId }
-    //         };
-    //     } 
-
-    //     try {
-    //         const requestUpdated = await this.request.update({
-    //             where: { id },
-    //             data
-    //         });
-
-    //         const requestData = await this.findOne( requestUpdated.id );
-
-    //         this.sseService.emitEvent({
-    //             message : requestData,
-    //             action  : EnumAction.UPDATE,
-    //             type    : Type.REQUEST,
-    //             origin
-    //         });
-
-    //         return requestData;
-    //     } catch ( error ) {
-    //         throw PrismaException.catch( error, 'Failed to update request' );
-    //     }
-    // }
+    #selectRequest = {
+        id              : true,
+        title           : true,
+        status          : true,
+        createdAt       : true,
+        updatedAt       : true,
+        staffCreate     : {
+            select: {
+                id          : true,
+                name        : true,
+                email       : true,
+                role        : true,
+                facultyId   : true
+            }
+        },
+        staffUpdate     : {
+            select: {
+                id      : true,
+                name    : true,
+                email   : true,
+                role    : true
+            }
+        },
+        section : {
+            select : {
+                id          : true,
+                code        : true,
+                startDate   : true,
+                endDate     : true,
+                isClosed    : true,
+                laboratory  : true,
+                workshop    : true,
+                lecture     : true,
+                professor   : {
+                    select: {
+                        id      : true,
+                        name    : true  
+                    }
+                },
+                period: {
+                    select : {
+                        id      : true,
+                        name    : true
+                    }
+                },
+                subject : {
+                    select : {
+                        id      : true,
+                        name    : true
+                    }
+                },
+                _count : {
+                    select: {
+                        sessions: true
+                    }
+                }
+            }
+        },
+        _count: {
+            select: {
+                requestSessions: true
+            }
+        },
+    }
 
 
-    // async remove( id: string, origin: string | undefined ) {
-    //     try {
-    //         const requestData = await this.findOne( id );
-    //         await this.request.delete({ where: { id } });
+    async create(
+        createRequestDto    : CreateRequestDto,
+        origin              : string | undefined
+    ) {
+        try {
+            const request = await this.request.create({
+                data    : createRequestDto,
+                select  : this.#selectRequest
+            });
 
-    //         this.sseService.emitEvent({
-    //             message : requestData,
-    //             action  : EnumAction.DELETE,
-    //             type    : Type.REQUEST,
-    //             origin
-    //         });
+            const requestData = this.#requestMap( request );
 
-    //         return requestData;
-    //     } catch ( error ) {
-    //         throw PrismaException.catch( error, 'Failed to delete request' );
-    //     }
-    // }
+            this.sseService.emitEvent({
+                message : requestData,
+                action  : EnumAction.CREATE,
+                type    : Type.REQUEST,
+                origin
+            });
+
+            return requestData;
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to create request' );
+        }
+    }
+
+
+    async findAll( facultyId: string ) {
+        const requests = await this.request.findMany({
+            select  : this.#selectRequest,
+            where   : {
+                staffCreate: {
+                    facultyId
+                }
+            }
+        });
+
+        return requests.map(( request ) => this.#requestMap( request ));
+    }
+
+
+    async findOne( sectionId: string ) {
+        const request = await this.request.findUnique({
+            select  : this.#selectRequest,
+            where   : { sectionId }
+        });
+
+        if ( !request ) {
+            throw new NotFoundException( 'Request not found' );
+        }
+
+        return this.#requestMap( request );
+    }
+
+
+
+    async findBySectionId( sectionId: string ) {
+        const request = await this.request.findUnique({
+            select : {
+                id              : true,
+                title           : true,
+                status          : true,
+                createdAt       : true,
+                updatedAt       : true,
+                staffCreate     : {
+                    select: {
+                        id          : true,
+                        name        : true,
+                        email       : true,
+                        role        : true,
+                        facultyId   : true
+                    }
+                },
+                staffUpdate     : {
+                    select: {
+                        id      : true,
+                        name    : true,
+                        email   : true,
+                        role    : true
+                    }
+                },
+                requestSessions : {
+                    select : {
+                        id              : true,
+                        session         : true,
+                        spaceId         : true,
+                        isEnglish       : true,
+                        isConsecutive   : true,
+                        description     : true,
+                        spaceType       : true,
+                        inAfternoon     : true,
+                        professor       : {
+                            select : {
+                                id : true,
+                                name : true
+                            }
+                        },
+                        spaceSize : {
+                            select : {
+                                id : true,
+                                detail : true
+                            }
+                        },
+                        staffUpdate : {
+                            select : {
+                                id : true,
+                                name : true
+                            }
+                        },
+                        sessionDayModules: {
+                            select: {
+                                id          : true,
+                                dayModule   : {
+                                    select      : {
+                                        dayId       : true,
+                                        module      : {
+                                            select      : {
+                                                id          : true,
+                                                code        : true,
+                                                startHour   : true,
+                                                endHour     : true,
+                                                difference  : true,
+                                                isActive    : true,
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            where   : { sectionId },
+        });
+
+        if ( !request ) {
+            throw new NotFoundException( 'Request not found' );
+        }
+
+        return this.#requestSectionMap( request );
+    }
+
+
+    async update(
+        id                  : string,
+        updateRequestDto    : UpdateRequestDto,
+        origin              : string | undefined
+    ) {
+        try {
+            const requestUpdated = await this.request.update({
+                select  : this.#selectRequest,
+                where   : { id },
+                data    : updateRequestDto
+            });
+
+            const requestData = this.#requestMap( requestUpdated );
+
+            this.sseService.emitEvent({
+                message : requestData,
+                action  : EnumAction.UPDATE,
+                type    : Type.REQUEST,
+                origin
+            });
+
+            return requestData;
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to update request' );
+        }
+    }
+
+
+    async remove( id: string, origin: string | undefined ) {
+        try {
+            await this.request.delete({
+                select: this.#selectRequest,
+                where: { id }
+            });
+
+            this.sseService.emitEvent({
+                message : true,
+                action  : EnumAction.DELETE,
+                type    : Type.REQUEST,
+                origin
+            });
+
+            return true;
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Failed to delete request' );
+        }
+    }
 
 }
