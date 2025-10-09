e35078dd9f95d83c9d9e8e94262859b44cfb2e48
kevinkeyssx <keyss1700@gmail.com>
2025-10-08 22:30:08 -0300

    sectionId field required

--- src/requests/dto/create-request.dto.ts
+++ src/requests/dto/create-request.dto.ts
@@ -1,4 +1,6 @@
-import { IntersectionType } from '@nestjs/swagger';
+import { ApiProperty, IntersectionType } from '@nestjs/swagger';
+
+import { IsString, Length } from 'class-validator';
 
 import { BasicRequestDto }  from '@requests/dto/basic-request.dto';
 import { StaffCreateIdDTO } from '@requests/dto/staff-create-id.dto';
@@ -7,4 +9,14 @@ import { StaffCreateIdDTO } from '@requests/dto/staff-create-id.dto';
 export class CreateRequestDto extends IntersectionType(
     BasicRequestDto,
     StaffCreateIdDTO
-) {}
+) {
+
+    @ApiProperty({ 
+        description : 'ID of the offer this request is for',
+        example     : '01H9XKJ8WXKJ8WXKJ8WXKJ8WX'
+    })
+    @IsString()
+    @Length( 24, 27 )
+    sectionId: string;
+
+}
