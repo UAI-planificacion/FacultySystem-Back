266887dce6b28f6feb5f162dce91dffc5b2cd557
kevinkeyssx <keyss1700@gmail.com>
2025-10-09 22:37:28 -0300

    Imports updated

--- src/sections/dto/update-group.dto.ts
+++ src/sections/dto/update-group.dto.ts
@@ -1,8 +1,8 @@
 import { IntersectionType } from '@nestjs/swagger';
 
-import { PeriodIdDto }  from '@sections/dto/fields/periodId.dto';
-import { SpaceSizeIdDto }      from '@sections/dto/fields/size.dto';
-import { CodeDto }      from '@sections/dto/fields/code.dto';
+import { PeriodIdDto }      from '@commons/dtos/periodId.dto';
+import { SpaceSizeIdDto }   from '@commons/dtos/size.dto';
+import { CodeDto }          from '@sections/dto/fields/code.dto';
 
 
 export class UpdateGroupDto extends IntersectionType(
