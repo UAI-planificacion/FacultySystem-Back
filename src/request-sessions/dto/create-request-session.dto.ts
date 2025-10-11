10213b85357d18f232ea138e4ef556b6c20ef597
kevinkeyssx <keyss1700@gmail.com>
2025-10-10 22:02:58 -0300

    ApiExtraModels implemented

--- src/request-sessions/dto/create-request-session.dto.ts
+++ src/request-sessions/dto/create-request-session.dto.ts
@@ -1,4 +1,7 @@
+import { ApiExtraModels } from '@nestjs/swagger';
+
 import { BasicRequestSessionDto } from "@request-sessions/dto/basic-request-session.dto";
 
 
+@ApiExtraModels()
 export class CreateRequestSessionDto extends BasicRequestSessionDto {}
