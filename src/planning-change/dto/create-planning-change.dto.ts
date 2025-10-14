0de07673e194fb9129a9c343bccd4d5f46e65f21
kevinkeyssx <keyss1700@gmail.com>
2025-10-13 21:16:45 -0300

    DayModulesIdDto implemented

--- src/planning-change/dto/create-planning-change.dto.ts
+++ src/planning-change/dto/create-planning-change.dto.ts
@@ -1,10 +1,13 @@
-import { IntersectionType } from "@nestjs/swagger";
+import { ApiExtraModels, IntersectionType } from "@nestjs/swagger";
 
+import { DayModulesIdDto }          from "@commons/dtos/day-moduleid.dto";
 import { BasicPlanningChangeDto }   from "@planning-change/dto/basic-planning-change.dto";
 import { StaffCreateIdDTO }         from "@requests/dto/staff-create-id.dto";
 
 
+@ApiExtraModels()
 export class CreatePlanningChangeDto extends IntersectionType(
     BasicPlanningChangeDto,
-    StaffCreateIdDTO
+    StaffCreateIdDTO,
+    DayModulesIdDto
 ) {}
