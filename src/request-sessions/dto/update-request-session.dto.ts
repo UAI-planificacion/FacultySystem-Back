632918f6f41d1db7e46bfad674b12901ed5ab102
kevinkeyssx <keyss1700@gmail.com>
2025-10-13 21:18:18 -0300

    StaffUpdateIdDTO removed *evaluate

--- src/request-sessions/dto/update-request-session.dto.ts
+++ src/request-sessions/dto/update-request-session.dto.ts
@@ -1,10 +1,6 @@
-import { IntersectionType, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 
 import { BasicRequestSessionDto }   from '@request-sessions/dto/basic-request-session.dto';
-import { StaffUpdateIdDTO }         from '@requests/dto/staff-update-id.dto';
 
 
-export class UpdateRequestSessionDto extends IntersectionType(
-    PartialType( BasicRequestSessionDto ),
-    StaffUpdateIdDTO
-) {}
+export class UpdateRequestSessionDto extends PartialType( BasicRequestSessionDto ) {}
