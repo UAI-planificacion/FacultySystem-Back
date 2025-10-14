9235cd7064860e7a6d282983223d97bc049c5fa8
kevinkeyssx <keyss1700@gmail.com>
2025-10-13 21:17:48 -0300

    DayModulesIdDto implemented

--- src/request-sessions/dto/create-request-session.dto.ts
+++ src/request-sessions/dto/create-request-session.dto.ts
@@ -1,7 +1,11 @@
-import { ApiExtraModels } from '@nestjs/swagger';
+import { ApiExtraModels, IntersectionType } from '@nestjs/swagger';
 
-import { BasicRequestSessionDto } from "@request-sessions/dto/basic-request-session.dto";
+import { DayModulesIdDto }          from '@commons/dtos/day-moduleid.dto';
+import { BasicRequestSessionDto }   from "@request-sessions/dto/basic-request-session.dto";
 
 
 @ApiExtraModels()
-export class CreateRequestSessionDto extends BasicRequestSessionDto {}
+export class CreateRequestSessionDto extends IntersectionType(
+    BasicRequestSessionDto,
+    DayModulesIdDto
+) {}
