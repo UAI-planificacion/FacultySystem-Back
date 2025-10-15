acaf91eadc79873141ba1e2f9642358bec33feff
kevinkeyssx <keyss1700@gmail.com>
2025-10-14 21:33:31 -0300

    SpacesService implemented

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { SessionsService }    from '@sessions/sessions.service';
 import { SessionsController } from '@sessions/sessions.controller';
 import { SectionsModule }     from '@sections/sections.module';
+import { SpacesService }      from '@commons/services/spaces.service';
 
 
 @Module({
 	imports		: [SectionsModule],
 	controllers	: [SessionsController],
-	providers	: [SessionsService],
+	providers	: [SessionsService, SpacesService],
 })
 export class SessionsModule {}
