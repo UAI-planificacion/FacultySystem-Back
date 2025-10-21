95a75d7f916618c75d080ba1bc40b3fe703112aa
kevinkeyssx <keyss1700@gmail.com>
2025-10-20 21:23:44 -0300

    update done & findSessionWhitouthPlanningChangeId & findOneBySessionId not working & selectPlanningChange & planingChangeMap implemented

--- src/planning-change/planning-change.service.ts
+++ src/planning-change/planning-change.service.ts
@@ -13,22 +13,170 @@ export class PlanningChangeService extends PrismaClient implements OnModuleInit 
         this.$connect();
     }
 
+
+    #selectPlanningChange = {
+        id              : true,
+        title           : true,
+        status          : true,
+        sessionName     : true,
+        building        : true,
+        spaceId         : true,
+        isEnglish       : true,
+        isConsecutive   : true,
+        description     : true,
+        spaceType       : true,
+        inAfternoon     : true,
+        isPriority      : true,
+        professor       : { 
+            select : {
+                name : true,
+                id : true
+            }
+        },
+        spaceSize     : {
+            select : {
+                id : true,
+                detail : true,
+            }
+        },
+        sessionId : true,
+        sectionId: true,
+        // session       : {
+        //     select : {
+        //         id          : true,
+        //         name        : true,
+        //         spaceId     : true,
+        //         date        : true,
+        //         isEnglish   : true,
+        //         professor   : {
+        //             select: {
+        //                 id      : true,
+        //                 name    : true
+        //             }
+        //         },
+        //         dayModule : {
+        //             select : {
+        //                 id      : true,
+        //                 dayId   : true,
+        //                 module  : {
+        //                     select : {
+        //                         id          : true,
+        //                         startHour   : true,
+        //                         endHour     : true,
+        //                         difference  : true,
+        //                         code        : true,
+        //                     }
+        //                 }
+        //             }
+        //         },
+        //     }
+        // },
+        // section       : {
+        //     select: {
+        //         id          : true,
+        //         code        : true,
+        //         isClosed    : true,
+        //         groupId     : true,
+        //         startDate   : true,
+        //         endDate     : true,
+        //         spaceType   : true,
+        //         spaceSizeId : true,
+        //         professor   : {
+        //             select: {
+        //                 id      : true,
+        //                 name    : true
+        //             }
+        //         },
+        //         spaceSize: {
+        //             select: {
+        //                 id      : true,
+        //                 detail  : true,
+        //             }
+        //         },
+        //         subject: {
+        //             select: {
+        //                 id      : true,
+        //                 name    : true,
+        //             }
+        //         },
+        //         period : {
+        //             select : {
+        //                 id      : true,
+        //                 name    : true,
+        //             }
+        //         },
+        //         workshop        : true,
+        //         lecture         : true,
+        //         tutoringSession : true,
+        //         laboratory      : true,
+        //     }
+        // },
+        createdAt       : true,
+        updatedAt       : true,
+        staffCreate       : {
+            select: {
+                id      : true,
+                name    : true,
+            }
+        },
+        staffUpdate: {
+            select: {
+                id      : true,
+                name    : true,
+            }
+        },
+        sessionDayModules: {
+            select: {
+                dayModuleId: true,
+                // dayModule: {
+                //     select: {
+                //         id          : true,
+                //         dayId       : true,
+                //         module      : {
+                //             select: {
+                //                 id          : true,
+                //                 startHour   : true,
+                //                 endHour     : true,
+                //                 difference  : true,
+                //                 code        : true,
+                //             }
+                //         }
+                //     }
+                // }
+            }
+        }
+    }
+
+
+    #planingChangeMap = ( planningChange: any ) => ({
+        ...planningChange,
+        dayModulesId: planningChange.sessionDayModules?.map(( dm ) => dm.dayModuleId ) ?? [],
+    })
+
+
     async create( createPlanningChangeDto: CreatePlanningChangeDto ) {
         try {
             const { dayModulesId, ...data } = createPlanningChangeDto;
 
-            const planningChange = await this.planningChange.create({
-                data
+            const cleanData = Object.fromEntries(
+                Object.entries( data ).filter(( [ _, value ] ) => value !== null )
+            ) as typeof data;
+
+            const planningChangeCreated = await this.planningChange.create({
+                select  : { id : true },
+                data    : cleanData
             });
 
-            await this.sessionDayModule.createMany({
-                data: dayModulesId.map(( dayModuleId ) => ({
-                    dayModuleId,
-                    planningChangeId: planningChange.id
-                })),
-            });
+            if ( dayModulesId.length > 0 ) {
+                await this.sessionDayModule.createMany({
+                    data: dayModulesId.map(( dayModuleId ) => ({
+                        dayModuleId,
+                        planningChangeId: planningChangeCreated.id
+                    })),
+                });
+            }
 
-            return planningChange;
+            return await this.findOne( planningChangeCreated.id );
         } catch ( error ) {
             throw PrismaException.catch( error, 'Failed to create planning change' );
         }
@@ -37,119 +185,122 @@ export class PlanningChangeService extends PrismaClient implements OnModuleInit 
 
     async findOne( id: string) {
         const planningChange = await this.planningChange.findUnique({
-            where: { id }
+            where: { id },
+            select: this.#selectPlanningChange
         })
 
         if ( !planningChange ) {
             throw new NotFoundException( `Planning change not found with id: ${id}` );
         }
 
-        return planningChange;
+        return this.#planingChangeMap ( planningChange );
     }
 
 
-    async findOneBySessionId( sessionId: string ) {
-        const planningChange = await this.planningChange.findUnique({
-            where: { sessionId }
-        })
+    // async findOneBySessionId( sessionId: string ) {
+    //     const planningChange = await this.planningChange.findUnique({
+    //         where: { sessionId }
+    //     })
 
-        if ( !planningChange ) {
-            throw new NotFoundException( 'Planning change not found' );
-        }
-
-        return planningChange;
-    }
-
-
-    async findSessionWhitouthPlanningChangeId() {
-        const planningChange = await this.session.findMany({
-            where: { planningChange: null },
-            select: {
-                id: true,
-                name: true,
-                date: true,
-                professor: {
-                    select: {
-                        id: true,
-                        name: true,
-                    }
-                },
-                dayModule: {
-                    select: {
-                        id: true,
-                        dayId: true,
-                        module : {
-                            select: {
-                                id: true,
-                                startHour: true,
-                                endHour: true,
-                                difference: true,
-                                code: true,
-                            }
-                        }
-                    }
-                },
-                spaceId: true,
-                isEnglish: true,
-                section: {
-                    select: {
-                        id: true,
-                        code: true,
-                        startDate: true,
-                        endDate: true,
-                        subject: {
-                            select: {
-                                id: true,
-                                name: true,
-                            }
-                        }
-                    }
-                },
-            }
-        });
-
-        return planningChange;
-    }
-
-    // async update( id: string, updatePlanningChangeDto: UpdatePlanningChangeDto ) {
-    //     try {
-    //         const { dayModulesId, ...data } = updatePlanningChangeDto;
-
-    //         const planningChange = await this.planningChange.update({
-    //             where : { id },
-    //             data
-    //         });
-
-
-    //         const dayModulesByPlanningChange = await this.sessionDayModule.findMany({
-	// 			where : { planningChangeId: id },
-	// 		});
-
-    //         const currentDayModuleIds   = dayModulesByPlanningChange.map(( dm ) => dm.dayModuleId ).sort();
-	// 		const newDayModuleIds       = ( dayModulesId || [] ).sort();
-	// 		const areEqual              = currentDayModuleIds.length === newDayModuleIds.length &&
-	// 			currentDayModuleIds.every(( id, index ) => id === newDayModuleIds[index] );
-
-    //         if ( !areEqual ) {
-    //             await this.sessionDayModule.deleteMany({
-    //                 where : { planningChangeId: id },
-    //             });
-
-    //             if ( newDayModuleIds.length > 0 ) {
-    //                 await this.sessionDayModule.createMany({
-    //                     data : newDayModuleIds.map(( dayModuleId ) => ({
-    //                         dayModuleId,
-    //                         planningChangeId: planningChange.id
-    //                     })),
-    //                 });
-    //             }
-    //         }
-
-    //         return planningChange;
-    //     } catch (error) {
-	// 		throw PrismaException.catch( error, 'Failed to update request session' );
+    //     if ( !planningChange ) {
+    //         throw new NotFoundException( 'Planning change not found' );
     //     }
+
+    //     return planningChange;
     // }
+
+
+    // async findSessionWhitouthPlanningChangeId() {
+    //     const planningChange = await this.session.findMany({
+    //         where: { planningChange: null },
+    //         select: {
+    //             id: true,
+    //             name: true,
+    //             date: true,
+    //             professor: {
+    //                 select: {
+    //                     id: true,
+    //                     name: true,
+    //                 }
+    //             },
+    //             dayModule: {
+    //                 select: {
+    //                     id: true,
+    //                     dayId: true,
+    //                     module : {
+    //                         select: {
+    //                             id: true,
+    //                             startHour: true,
+    //                             endHour: true,
+    //                             difference: true,
+    //                             code: true,
+    //                         }
+    //                     }
+    //                 }
+    //             },
+    //             spaceId: true,
+    //             isEnglish: true,
+    //             section: {
+    //                 select: {
+    //                     id: true,
+    //                     code: true,
+    //                     startDate: true,
+    //                     endDate: true,
+    //                     subject: {
+    //                         select: {
+    //                             id: true,
+    //                             name: true,
+    //                         }
+    //                     }
+    //                 }
+    //             },
+    //         }
+    //     });
+
+    //     return planningChange;
+    // }
+
+
+    async update( id: string, updatePlanningChangeDto: UpdatePlanningChangeDto ) {
+        try {
+            const { dayModulesId, ...data } = updatePlanningChangeDto;
+
+            const planningChange = await this.planningChange.update({
+                where   : { id },
+                select  : { id : true },
+                data
+            });
+
+            const dayModulesByPlanningChange = await this.sessionDayModule.findMany({
+				where : { planningChangeId: id },
+			});
+
+            const currentDayModuleIds   = dayModulesByPlanningChange.map(( dm ) => dm.dayModuleId ).sort();
+			const newDayModuleIds       = ( dayModulesId || [] ).sort();
+			const areEqual              = currentDayModuleIds.length === newDayModuleIds.length &&
+				currentDayModuleIds.every(( id, index ) => id === newDayModuleIds[index] );
+
+            if ( !areEqual ) {
+                await this.sessionDayModule.deleteMany({
+                    where : { planningChangeId: id },
+                });
+
+                if ( newDayModuleIds.length > 0 ) {
+                    await this.sessionDayModule.createMany({
+                        data : newDayModuleIds.map(( dayModuleId ) => ({
+                            dayModuleId,
+                            planningChangeId: planningChange.id
+                        })),
+                    });
+                }
+            }
+
+            // return planningChange;
+            return this.findOne( planningChange.id );
+        } catch (error) {
+			throw PrismaException.catch( error, 'Failed to update request session' );
+        }
+    }
 
     async remove(id: string) {
         try {
