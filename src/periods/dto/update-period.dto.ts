9413838582388e355020548f2ae6d12afaec368c
kevinkeyssx <keyss1700@gmail.com>
2025-11-03 21:55:58 -0300

    $Enums.PeriodStatus implemented

--- src/periods/dto/update-period.dto.ts
+++ src/periods/dto/update-period.dto.ts
@@ -1,26 +1,21 @@
 import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 
-import { IsEnum, IsOptional } from 'class-validator';
+import { $Enums }               from 'generated/prisma';
+import { IsEnum, IsOptional }   from 'class-validator';
 
 import { CreatePeriodDto } from '@periods/dto/create-period.dto';
 
 
-export enum PeriodStatusDto {
-    InProgress = 'InProgress',
-    Closed = 'Closed',
-}
-
-
-export class UpdatePeriodDto extends PartialType(CreatePeriodDto) {
+export class UpdatePeriodDto extends PartialType( CreatePeriodDto ) {
 
     @ApiPropertyOptional({
         description: 'The status of the period.',
-        enum: PeriodStatusDto,
-        example: PeriodStatusDto.InProgress,
-        default: PeriodStatusDto.InProgress,
+        enum: $Enums.PeriodStatus,
+        example: $Enums.PeriodStatus.InProgress,
+        default: $Enums.PeriodStatus.InProgress,
     })
     @IsOptional()
-    @IsEnum(PeriodStatusDto)
-    status?: PeriodStatusDto = PeriodStatusDto.InProgress;
+    @IsEnum($Enums.PeriodStatus)
+    status?: $Enums.PeriodStatus = $Enums.PeriodStatus.InProgress;
 
 }
