5a5b6431dbfd03fddb040b89edf812056645ce15
kevinkeyssx <keyss1700@gmail.com>
2025-11-07 22:16:53 -0300

    ExcelSessionDto It's not ready yet.

--- src/sessions/interfaces/excelSession.dto.ts
+++ src/sessions/interfaces/excelSession.dto.ts
@@ -1,6 +1,7 @@
 export interface ExcelSessionDto {
     SSEC            : string;
     sessionId       : string;
+    dayModuleId     : string;
     spaceId?        : string;
     professorId?    : string;
 }
