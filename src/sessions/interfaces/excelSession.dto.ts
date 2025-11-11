89f420cbb457531a7d47ffa4a8a1afe1a31096ed
kevinkeyssx <keyss1700@gmail.com>
2025-11-11 01:05:53 -0300

    ExcelSessionDto & SessionDataDto & SessionAvailabilityResult implemented

--- src/sessions/interfaces/excelSession.dto.ts
+++ src/sessions/interfaces/excelSession.dto.ts
@@ -1,7 +1,45 @@
-export interface ExcelSessionDto {
+export class ExcelSessionDto {
     SSEC            : string;
-    sessionId       : string;
-    dayModuleId     : string;
-    spaceId?        : string;
-    professorId?    : string;
+    SesionId        : string;
+    Numero          : number;
+    NombreAsignatura: string;
+    Dia             : number;
+    Modulo          : string;
+    Periodo         : string;
+    TipoPeriodo     : string;
+    Edificio        : string | null;
+    TipoEspacio     : string | null;
+    TamanoEspacio   : string | null;
+    TipoSesion      : string;
+    Cupos           : number;
+    Profesor        : string | null;
+    Espacio         : string | null;
+}
+
+
+export interface Professor {
+    id      : string;
+    name    : string;
+}
+
+export class SessionDataDto {
+    sessionId   : string;
+    spaceId?    : string;       // Solo si type === 'space'
+    professor?  : Professor;    // Solo si type === 'professor'
+}
+
+
+type Status = 'Available' | 'Unavailable' | 'Probable';
+
+
+export interface SessionAvailabilityResult {
+    SSEC        : string;
+    session     : string;
+    date        : Date;
+    module      : string;
+    spaceId?    : string;
+    professor?  : Professor;
+    status      : Status;
+    detalle     : string;
+    sessionId   : string;
 }
