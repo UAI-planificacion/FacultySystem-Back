e0ddde27816f2e1205cdd4e383f836c5a16c48db
kevinkeyssx <keyss1700@gmail.com>
2025-11-17 23:07:01 -0300

    SseModule imported

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { CommentsService }      from '@comments/comments.service';
 import { CommentsController }   from '@comments/comments.controller';
+import { SseModule }            from '@sse/sse.module';
 
 
 @Module({
     controllers : [ CommentsController ],
     providers   : [ CommentsService ],
+    imports     : [ SseModule ]
 })
 export class CommentsModule {}
