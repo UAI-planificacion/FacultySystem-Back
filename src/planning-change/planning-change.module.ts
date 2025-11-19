f4af26514406f42302462f360a194ee2cf5c9004
kevinkeyssx <keyss1700@gmail.com>
2025-11-18 22:07:48 -0300

    SseModule imported

--- src/planning-change/planning-change.module.ts
+++ src/planning-change/planning-change.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { PlanningChangeService }    from '@planning-change/planning-change.service';
 import { PlanningChangeController } from '@planning-change/planning-change.controller';
+import { SseModule }                from '@sse/sse.module';
 
 
 @Module({
     controllers : [PlanningChangeController],
     providers   : [PlanningChangeService],
+    imports     : [SseModule],
 })
 export class PlanningChangeModule {}
