aa5a2d0be85de619a7ee629399031a64582521a7
kevinkeyssx <keyss1700@gmail.com>
2025-11-27 22:57:17 -0300

    SpacesService in providers implemented

--- src/sections/sections.module.ts
+++ src/sections/sections.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { SectionsService }		from '@sections/sections.service';
 import { SectionsController }   from '@sections/sections.controller';
+import { SpacesService }		from '@commons/services/spaces.service';
 
 
 @Module({
 	controllers	: [SectionsController],
-	providers	: [SectionsService],
+	providers	: [SectionsService, SpacesService],
 	exports		: [SectionsService],
 })
 export class SectionsModule {}
